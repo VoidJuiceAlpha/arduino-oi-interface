0703d2f1a3c1cf294563316cab21e79a6e329bf9
shiman6 <shiman6@gmail.com>
2017-08-14 16:31:31 -0700

    readme edits

--- android_oi_source/android_oi_source.ino
+++ android_oi_source/android_oi_source.ino
@@ -1,1 +1,26 @@
+/*Android OI Interface
 
+This is an interface to make it a *little* for a more complex system
+to communicate with the iRobot Create. Think of it like subconcious 
+motor control
+
+*/
+
+#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
+#include <stdin.h>
+
+SoftwareSerial oiSerial(/*pins here*/) // Recieve pin, transmit pin
+
+void setup()
+{
+  Serial.begin(57600); //change to good rate for RPi
+  
+  oiSerial.begin(4800); //Change to robot rate
+}
+
+void loop()
+{
+  
+  
+}
